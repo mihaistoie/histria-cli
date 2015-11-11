e62bea92c82380ae7b6ffd1d54d47df6a322d64c
misto <mihaistoie@gmail.com>
2015-11-11 16:01:19 +0100

    #

--- src/core/core.ts
+++ src/core/core.ts
@@ -2,25 +2,39 @@
 namespace Phoenix {
     export module utils {
         var
-            _createPromise = function(resolve: (value?: any | Thenable<any>) => void, reject: (error?: any) => void): Promise<any> {
-                let p: any = (window['Promise'] ? window['Promise'] : (window['ES6Promise'] ? window['ES6Promise'].Promise : null));
-                return <Promise<any>>new p(resolve, reject);
+            _copyArray = function(ss: any[], recursive): any[] {
+                let res = new Array(ss.length);
+                ss.forEach(function(item, index) {
+                    if (recursive && Array.isArray(item)) {
+                        res[index] = _copyArray(item, true);
+                    } else if (recursive && typeof item === 'object') {
+                        res[index] = _copyObject(item, true);
+                    } else
+                        res[index] = item;
+                });
+                return res;
             },
-            _p8 = function(addSeparator: boolean): string {
-                var p = (Math.random().toString(16) + "000000000").substr(2, 8);
-                return addSeparator ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
-            },
-            _createID = function(): string {
-                return "I" + _p8(false) + _p8(false) + _p8(false) + _p8(false);
-            },
-            _genUuid = function(): string {
-                return _p8(false) + _p8(true) + _p8(true) + _p8(false);
+            _copyObject = function(src: any, recursive): any {
+                let res = {};
+                Object.keys(src).forEach(function(pn) {
+                    let item = src[pn];
+                    if (recursive && Array.isArray(item)) {
+                        res[pn] = _copyArray(item, true);
+                    } else if (recursive && typeof item === 'object') {
+                        res[pn] = _copyObject(item, true);
+                    } else
+                        res[pn] = item;
+                });
+                return res;
             },
             _extend = function(dst: any, src: any, recursive?: boolean): any {
-                Object.keys(src).forEach(function(pn) {
-                    dst[pn] = src[pn];
-                });
-                return dst;
+                if (!src) return src;
+                if (Array.isArray(src)) {
+                    return _copyArray(src, recursive);
+                } else if (typeof src === 'object') {
+                    return _copyObject(src, recursive);
+                } else
+                    return src;
             },
             _formatByPosition = function(...args: any[]): string {
                 let arg0 = args[0] + '';
@@ -33,8 +47,25 @@ namespace Phoenix {
                 return value.replace(/{(.*)}/g, function(match, val) {
                     return params[val] || '';
                 });
+            },
+            _createPromise = function(resolve: (value?: any | Thenable<any>) => void, reject: (error?: any) => void): Promise<any> {
+                let p: any = (window['Promise'] ? window['Promise'] : (window['ES6Promise'] ? window['ES6Promise'].Promise : null));
+                return <Promise<any>>new p(resolve, reject);
+            },
+            _p8 = function(addSeparator: boolean): string {
+                var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+                return addSeparator ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+            },
+            _genUuid = function(): string {
+                return _p8(false) + _p8(true) + _p8(true) + _p8(false);
+            },
+            _createID = function(): string {
+                return "H" + _p8(false) + _p8(false) + _p8(false) + _p8(false);
             };
         export var createPromise = _createPromise;
+        export var extend = _extend;
+        export var uuid = _genUuid;
+        export var allocId = _createID;
 
     }
 }
