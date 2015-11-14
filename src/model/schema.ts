03e4f2d680654025a73e80cc98df3b424957d81d
misto <mihaistoie@gmail.com>
2015-11-14 13:24:03 +0100

    #

---
+++ src/model/schema.ts
@@ -0,0 +1,30 @@
+/// <reference path="../core/core.ts" />
+namespace Histria {
+	export module Schema {
+		var
+			_utils = utils;
+		var
+			_isRefObject = function(prop): boolean {
+				return prop.type === "ref/object";
+			},
+			_isRefArray = function(prop): boolean {
+				return prop.type === "ref/array";
+			},
+			_isObject = function(prop): boolean {
+				return prop.type === "object";
+			},
+			_isArray = function(prop): boolean {
+				return prop.type === "array";
+			},
+			_enumProps = function(schema: any, cb: (value: any, isObject: boolean, isArray: boolean) => void): void {
+				Object.keys(schema.properties).forEach(function(propName: string) {
+					let item = schema.properties[propName];
+					if (_isRefObject(item) || _isRefArray(item)) return;
+					cb(item, _isObject(item), _isArray(item));
+				});
+
+			};
+		export const enumProperties = _enumProps;
+	}
+
+}
