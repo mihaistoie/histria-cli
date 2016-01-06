7e2127fd66e38334628d5b18eed74b3c3914a294
Mihai  STOIE <mihaistoie@gmail.com>
2016-01-06 11:12:25 +0100

    #

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 'use strict'
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 
 gulp.task('default', function () {
     return gulp.src('src/**/*.ts')
         .pipe(ts({
             noImplicitAny: false,
-            out: 'output.js',
-            target: 'ES5' 
+            target: 'ES5'
         }))
-        .pipe(gulp.dest('dist/js'));
+		.pipe(concat("histria.js"))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename("histria.min.js"))
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('dist/js'));
+		
+        
+		
 });
-
